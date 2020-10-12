f711fd93a7431a1ca100e1bdb3fdbbd4f6cd54b2
glorial <glorial@naver.com>
2020-10-12 09:32:09 +0900

    json, querystring 타입으로 thymeleaf 양식 변환처리(json은 실패함)

--- src/main/java/kr/co/study/web/controller/MainController.java
+++ src/main/java/kr/co/study/web/controller/MainController.java
@@ -2,11 +2,17 @@ package kr.co.study.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.study.application.MainService;
+import kr.co.study.web.dto.MainDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MainController {
@@ -18,5 +24,23 @@ public class MainController {
 		model.addAttribute("hello", "Hello World");
 		model.addAttribute("userCount", service.getUserCount());
 		return "thymeleaf/page/index";
+	}
+
+	@RequestMapping(value = "/ajaxTest", method = RequestMethod.POST)
+	public String ajaxTest(Model model, @RequestBody MainDTO dto) {
+		log.debug("dto : {}", dto);
+
+		model.addAttribute("mainDTO", dto);
+
+		return "thymeleaf/page/index :: #boardList";
+	}
+
+	@RequestMapping(value = "/formTest", method = RequestMethod.POST)
+	public String formTest(Model model, @ModelAttribute MainDTO dto) {
+		log.debug("dto : {}", dto);
+
+		model.addAttribute("mainDTO", dto);
+
+		return "thymeleaf/page/index :: #boardList";
 	}
 }
---
+++ src/main/java/kr/co/study/web/dto/MainDTO.java
@@ -0,0 +1,16 @@
+package kr.co.study.web.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class MainDTO {
+	private String searchText;
+}
