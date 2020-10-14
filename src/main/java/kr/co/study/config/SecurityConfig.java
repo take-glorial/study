9735ddfdf2a61d91771e3cb65026102926670e46
take <take004@take0801.onmicrosoft.com>
2020-10-14 15:59:01 +0900

    security 허용할 페이지 추가

--- src/main/java/kr/co/study/config/SecurityConfig.java
+++ src/main/java/kr/co/study/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 
 			.authorizeRequests()
+				.antMatchers("write-down-view-names").permitAll()
 				.anyRequest().authenticated()
 			.and()
 
